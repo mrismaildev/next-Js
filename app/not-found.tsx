e1a78c5731b4e90d97b55e610764c191232b948d
mrismaildev <ismail.dev.khulna@gmail.com>
2025-11-13 15:28:14 +0600

    create 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notFound = () => {
+  return (
+    <div className="notFOund">
+      <p>Ismail 404 | not found</p>
+    </div>
+  );
+};
+
+export default notFound;
